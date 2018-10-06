db3569c59942c47c485707021be977884b4ce9c6
Maksim Ivanov <maksim.ivanov@altenar.com>
2018-10-06 20:11:27 +0300

    updated pages, layouts, styles

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <nuxt-link to="/">Home</nuxt-link>
-    <nuxt-link to="/about">About</nuxt-link>
+  <div class="bg-blue-darkest font-semibold">
+    <div class="container mx-auto flex items-center py-6">
+      <nuxt-link to="/" 
+        class="text-white no-underline hover:text-grey-light mr-8">Home</nuxt-link>
+      <nuxt-link to="/about"
+        class="text-white no-underline hover:text-grey-light mr-8">About</nuxt-link>
+    </div>
   </div>
 </template>
